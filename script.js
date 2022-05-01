e97f0c6cc25e1130a97942b531cfe9817a93285a
Fran <francisco.chiarino@sofatutor.com>
2022-05-01 12:17:15 +0200

    Add reload after 5 minutes from new find
    Once a flat is found, wait 5 min and reload the page again

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 {
-  console.log('Looking for a new flat...');
   document.getElementById('partners_wrapper').style.display = 'none';
 
-  const lastPostIsRecent = () => {
+  const isLastPostRecent = () => {
     const uploadTimeText = document.querySelector('.offer_list_item .row .card_body .bottom .col-xs-9 .row:last-child .col-sm-12 span:last-child').innerText;
     const timeSinceUpload = parseInt(uploadTimeText.split(' ')[1]);
 
-    return timeSinceUpload < 5 && uploadTimeText.includes('Minuten') ||
+    return (
+      timeSinceUpload < 5 && uploadTimeText.includes('Minuten') ||
       uploadTimeText.includes('Online: 1 Minute') ||
       uploadTimeText.includes('Sekunden')
+    )
   }
 
   const triggerNotification = () => {
+    const fiveMinutes = 300000;
     const port = chrome.extension.connect({
       name: "notification request"
     });
+
     port.postMessage("triggerNotification");
+    setTimeout(() => location.reload(), fiveMinutes);
   }
 
   setTimeout(() => {
-    if (lastPostIsRecent()) {
+    if (isLastPostRecent()) {
       triggerNotification();
     } else {
       console.log('No new uploaded, will reload the page...')
